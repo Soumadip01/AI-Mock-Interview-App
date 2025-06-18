ce65de35f619954c2a954c0780e168b35fbaf6c0
Soumadip <soumadiphalder26@gmail.com>
2025-06-19 00:49:25 +0530

    Project init and Setup with ShadCN UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+      <div>Hello</div>
+      <Button>Fuck ME</Button>
+    </div>
+  )
+}
+
+export default App
